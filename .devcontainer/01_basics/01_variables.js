7846aae9ef28343ff791c2aedeca2ec1e713bb4e
saketraaz <111704524+saketraaz@users.noreply.github.com>
2024-08-06 13:50:02 +0000

    data types

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID=144553
+let accountemail="helllo@gmail.com"
+var accountpassword="@12345"
+accountcity="Patna"
+let accountstate;
+
+//accountID=2 
+//not allowed changing constants
+accountemail="saket@123.com"
+accountpassword="banking"
+accountcity="railway"
+console.log(accountID);
+console.table([accountID, accountcity, accountemail, accountpassword, accountstate]);
+ 
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";
+//treats all code as newer version
+//alert("hello") we are using node js not browser
+console.log(3+3)
+console.log("saket raj") 
+
+let name="hello"
+let age=22
+let flag=true;
+
+// number
+// bigint
+// string
+// boolean
+// symbol => uniqueness
+// null
+// undefined
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined) //  undefined
