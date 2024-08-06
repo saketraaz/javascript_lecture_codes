1411c7b58e87b0d6d2e1714cac3ce3ee40e077ce
saketraaz <111704524+saketraaz@users.noreply.github.com>
2024-08-06 14:14:11 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversion.js
@@ -0,0 +1,25 @@
+let score="30abc";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let value=Number (score);
+
+console.log(typeof value);
+console.log(value);
+
+// conversion from number to string and vice versa
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined => NaN
+// true => 1
+// false => 0
+
+console.log("Checkers");
+let isloggedin = "";
+let isLoggedIn = Boolean (isloggedin);
+console.log(isLoggedIn);
+
+// "" => false
+// "saket" => true 
