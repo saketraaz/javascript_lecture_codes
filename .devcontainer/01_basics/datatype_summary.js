646793fe51e0ca2c012c3b242d241197a56818f3
saketraaz <111704524+saketraaz@users.noreply.github.com>
2024-08-08 10:35:13 +0000

    summary done

--- .devcontainer/01_basics/datatype_summary.js
+++ .devcontainer/01_basics/datatype_summary.js
@@ -1,1 +1,20 @@
 //in JS data types are categorized in two types on the basis of the way data is stores and accessed from the memory
+
+
+let myname="SaketRaj";
+let anothername=myname;
+anothername="Golu";
+console.log(myname);
+console.log(anothername);
+
+let userOne = 
+{
+    email:"saket@123", 
+    upi: "user@ybl"
+}
+
+let userTwo = 
+{
+    email:"saket@123", 
+    upi: "user@ybl"
+}
