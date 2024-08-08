66cf67c243df96dc6ae2b8d1d0f6af3ed5970b3c
saketraaz <111704524+saketraaz@users.noreply.github.com>
2024-08-08 09:34:31 +0000

    comparison done

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+console.log("part1")
+console.log(2>1);
+console.log(2>=1);
+console.log(2==2);
+console.log(3<=1);
+
+console.log("part2")
+console.log("2">1);
+console.log("02">1);
+
+console.log("part3");
+console.log(null <0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log("part4");
+console.log(2==="2");
