14326453ad451d64f5196e57215cb9330ddfb2ad
saketraaz <111704524+saketraaz@users.noreply.github.com>
2024-08-08 12:35:25 +0000

    string done

---
+++ .devcontainer/01_basics/06_string.js
@@ -0,0 +1,25 @@
+const name="saket";
+const repocount=50;
+
+console.log(`Hello my name is ${name} and my repocount is ${repocount}`);
+// using ` character to replace values from the variable
+
+ const gamename=new String('Saket-RajkaGame');
+ console.log(gamename[0]);
+ console.log(gamename.__proto__);
+
+
+ console.log(gamename.length);
+ console.log(gamename.toUpperCase());
+ console.log(gamename.charAt(4));
+ console.log(gamename.indexOf('x'));
+ console.log(gamename.indexOf('x'));
+ const newstring=gamename.substring(0, 4);
+ console.log(newstring);
+
+ const anotherstring=gamename.slice(-7, 4);
+ console.log(anotherstring);
+
+ const str1="    hello";
+ console.log(str1);
+ console.log(str1.trim());
