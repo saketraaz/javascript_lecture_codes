5b81dcf41970acc992cc02c5c3a887d942762301
saketraaz <111704524+saketraaz@users.noreply.github.com>
2024-08-18 13:43:28 +0000

    array 2

---
+++ .devcontainer/02_basics/02_arrays.js
@@ -0,0 +1,15 @@
+const arr1=["ironman", "thor", "cap"];
+const arr2=["superman", "joker", "batman"];
+
+//arr1.push(arr2);
+console.log(arr1);
+const arr3=arr1.concat(arr2);
+console.log("C", arr3);
+console.log('\n');
+const new_heroes=[...arr1, ...arr2];
+console.log(new_heroes);
+
+let c1=100;
+let c2=200;
+let c3=300;
+console.log(Array.of(c1, c2, c3));
